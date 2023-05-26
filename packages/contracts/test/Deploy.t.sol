67881e0184b500a88d377682cd5fec75e9b98abb
pwrstudio <pwrpntr@gmail.com>
2023-05-26 10:38:09 +0200

    fix(contracts): initialize indices

--- packages/contracts/test/Deploy.t.sol
+++ packages/contracts/test/Deploy.t.sol
@@ -2,7 +2,9 @@
 pragma solidity >=0.8.17;
 import { console } from "forge-std/console.sol";
 import { MudV2Test } from "./MudV2Test.t.sol";
+import { BodyOne, BodyTwo, MatchKey } from "../src/constants.sol";
 import "../src/codegen/Tables.sol";
+import "../src/libraries/Libraries.sol";
 
 contract DeployTest is MudV2Test {
   function testWorldExists() public {
@@ -19,5 +21,20 @@ contract DeployTest is MudV2Test {
     setUp();
     vm.expectRevert(bytes("already initialized"));
     world.moving_castles_MatchSystem_init();
+  }
+
+  function testIndicesInitialized() public {
+    setUp();
+
+    vm.startPrank(alice);
+    world.moving_castles_SpawnSystem_spawn("Alice");
+    vm.stopPrank();
+
+    bytes32 coreEntity = LibUtils.addressToEntityKey(alice);
+
+    assertEq(MatchIndex.get(world, MatchKey), 0);
+    assertEq(RoundIndex.get(world, coreEntity), 0);
+    assertEq(RoundIndex.get(world, BodyOne), 0);
+    assertEq(RoundIndex.get(world, BodyTwo), 0);
   }
 }
